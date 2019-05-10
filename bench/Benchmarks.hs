d5124a04457740b913d3c0e778cdfd7f91adfa4e
ilyasridhuan <ilyasridhuan@gmail.com>
2019-05-10 11:54:41 +0800

    Added Benchmarks to Aggregated RangeProof

--- bench/Benchmarks.hs
+++ bench/Benchmarks.hs
@@ -13,11 +13,11 @@ main :: IO ()
 main = do
     h <- generateQ crv <$> scalarGenerate crv
     rp <- generateQ crv <$> scalarGenerate crv
-    let vBlind = 10
-        v = 8
-        commV = pointAdd crv (pointMul crv vBlind h) (pointBaseMul crv (toInteger v))
-    range_proof <- generate_range_proof v vBlind h rp
-    let curriedVerify = (\x -> verify_range_proof x commV h rp)
+    let vBlinds = [10]
+        vs = [8]
+        commVs = (\(v,vBlind) -> pointAdd crv (pointMul crv vBlind h) (pointBaseMul crv (toInteger v))) <$> zip vs vBlinds
+    range_proof <- generate_range_proof vs vBlinds h rp
+    let curriedVerify = (\x -> verify_range_proof x commVs h rp)
     -- verified <- verify_range_proof range_proof commV h rp
     
     defaultMain [
